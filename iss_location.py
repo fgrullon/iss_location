9b5f8a369de5f33d381cd0741b52f5f069b61523
Frank <franfelix@gmail.com>
2018-02-02 09:38:40 -0400

    Script

---
+++ iss_location.py
@@ -0,0 +1,19 @@
+import requests
+from geopy.geocoders import Nominatim #geopy was downloaded from github
+
+iss = requests.get(url ="http://api.open-notify.org/iss-now.json") #getting coordinates of ISS from API
+iss = iss.json()
+latitude, longitude = (iss['iss_position']['latitude'], iss['iss_position']['longitude'])
+
+geolocator = Nominatim()
+
+def issCountryLocator(latitude,longitude):
+    ll = (str(latitude) +", "+str(longitude))
+    location = geolocator.reverse(ll)
+    if type(location.address) == str:
+         print(location.address)
+    else:
+       print("The ISS is currently over water")
+
+
+issCountryLocator(latitude,longitude)
